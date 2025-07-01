98ccf1a5bc930f89d97bde6855ebfe135d903812
Luiz Mendonça <luizeduardomendonca77@gmail.com>
2025-07-01 00:03:30 -0300

    fix erros de nomeação e add infos

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,6 @@
 import { BsSearch } from "react-icons/bs"
-import styles from './home.module.css'
+import styles from './Home.module.css'
+import { Link } from "react-router"
 
 
 export function Home(){
@@ -14,6 +15,47 @@ export function Home(){
                     <BsSearch size={30} color="#FFF" />
                 </button>
             </form>
+
+            <table>
+                <thead>
+                    <tr>
+                    <th scope="col">Moeda</th>
+                    <th scope="col">Valor mercado</th>
+                    <th scope="col">Preço</th>
+                    <th scope="col">Volume</th>
+                    <th scope="col">Mudança 24h</th>
+                    </tr>
+                </thead>
+
+                <tbody id="tbody">
+                    <tr className={styles.tr}>
+
+                    <td className={styles.tdLabel} data-label="Moeda">
+                        <div className={styles.name}>
+                            <Link to={"/detail/bitcoin"}>
+                                <span>Bitcoin</span> | BTC
+                            </Link>
+                    </div>
+                    </td>
+                    <td className={styles.tdLabel} data-label="Valor mercado">
+                    1T
+                    </td>
+                    <td className={styles.tdLabel} data-Label="Preço">
+                    8.000
+                    </td>
+                    <td className={styles.tdLabel} data-Label="Volume">
+                    2B
+                    </td>
+                    <td className={styles.tdLabel} data-Label="Volume">
+                    2B
+                    </td>
+                    <td className={styles.tdLabel}  data-Label="Mudança 24h">
+                     <span>1.20</span>
+                    </td>
+
+                </tr>
+            </tbody>
+           </table>
         </main>
     )
 }
