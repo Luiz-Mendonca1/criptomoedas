95fd6caa965401a4659a4bbc50402640e04b9929
Luiz Mendonça <luizeduardomendonca77@gmail.com>
2025-07-01 09:50:38 -0300

    add desing e responsavidade na home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,61 +1,66 @@
-import { BsSearch } from "react-icons/bs"
-import styles from './Home.module.css'
-import { Link } from "react-router"
+import styles from './home.module.css'
+import { BsSearch } from  'react-icons/bs'
+import { Link } from 'react-router-dom'
+
+export function Home() {
+
+  return (
+    <main className={styles.container}>
+      <form className={styles.form}>
+        <input 
+          type="text"
+          placeholder="Digite o nome da moeda... EX bitcoin"
+        />
+        <button type="submit">
+          <BsSearch size={30} color="#FFF" />
+        </button>
+      </form>
 
 
-export function Home(){
-    return (
-        <main className={styles.container}>
-            <form className={styles.form}>
-                <input
-                    type="text"
-                    placeholder="Digite o nome da moeda... EX bitcoin"
-                />
-                <button type="submit">
-                    <BsSearch size={30} color="#FFF" />
-                </button>
-            </form>
+      <table>
+        <thead>
+          <tr>
+            <th scope="col">Moeda</th>
+            <th scope="col">Valor mercado</th>
+            <th scope="col">Preço</th>
+            <th scope="col">Volume</th>
+            <th scope="col">Mudança 24h</th>
+          </tr>
+        </thead>
 
-            <table>
-                <thead>
-                    <tr>
-                    <th scope="col">Moeda</th>
-                    <th scope="col">Valor mercado</th>
-                    <th scope="col">Preço</th>
-                    <th scope="col">Volume</th>
-                    <th scope="col">Mudança 24h</th>
-                    </tr>
-                </thead>
+        <tbody id="tbody">
 
-                <tbody id="tbody">
-                    <tr className={styles.tr}>
+          <tr className={styles.tr}>
 
-                    <td className={styles.tdLabel} data-label="Moeda">
-                        <div className={styles.name}>
-                            <Link to={"/detail/bitcoin"}>
-                                <span>Bitcoin</span> | BTC
-                            </Link>
-                    </div>
-                    </td>
-                    <td className={styles.tdLabel} data-label="Valor mercado">
-                    1T
-                    </td>
-                    <td className={styles.tdLabel} data-Label="Preço">
-                    8.000
-                    </td>
-                    <td className={styles.tdLabel} data-Label="Volume">
-                    2B
-                    </td>
-                    <td className={styles.tdLabel} data-Label="Volume">
-                    2B
-                    </td>
-                    <td className={styles.tdLabel}  data-Label="Mudança 24h">
-                     <span>1.20</span>
-                    </td>
+            <td className={styles.tdLabel} data-label="Moeda">
+              <div className={styles.name}>
+                <Link to={"/detail/bitcoin"}>
+                  <span>Bitcoin</span> | BTC
+                </Link>
+              </div>
+            </td>
 
-                </tr>
-            </tbody>
-           </table>
-        </main>
-    )
+            <td className={styles.tdLabel} data-label="Valor mercado">
+              1T
+            </td>
+
+            <td className={styles.tdLabel} data-label="Preço">
+              8.000
+            </td>
+
+            <td className={styles.tdLabel} data-label="Volume">
+              2B
+            </td>
+
+            <td className={styles.tdProfit} data-label="Mudança 24h">
+              <span>1.20</span>
+            </td>
+
+          </tr>
+
+        </tbody>
+      </table>
+
+    </main>
+  )
 }
